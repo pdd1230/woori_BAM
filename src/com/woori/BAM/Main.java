f8d1694c8493e8246d0adbe2e71a276fbf9c0a09
pdd1230 <pddlim@gmail.com>
2025-04-28 11:03:36 +0900

    article modify 객체 주소 보사 이용

--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
+		
 		System.out.println("== 프로그램 시작 ==");
 		Scanner sc = new Scanner(System.in); 
        
@@ -21,7 +22,7 @@ public class Main {
 				System.out.println("명령어를 입력해 주세요");
 				continue;
 			}
-			// List 메서드중 size() 이용 (Data 유무를 객체의 객수(크기)로 변환)
+
 			if (cmd.equals("article write")) {
 		
 				System.out.printf("제목 : ");
@@ -43,14 +44,14 @@ public class Main {
 					
 				}
 				
-				System.out.printf("번호    |     제목\n");
+				System.out.printf("번호    |     제목   |    내용\n");
 				for (int i = articles.size() -1 ; i >= 0; i--) {
 					Article article = articles.get(i);
-					System.out.printf("%d       |     %s\n", article.id, article.title);
+					System.out.printf("%d      |     %s   |   %s\n", article.id, article.title, article.body);
 				}
 				
-			} else if (cmd.startsWith("article detail ")) { // article detail 로 시작하니?
-				String[] cmdBits = cmd.split(" ");  // 번호 ==> cmdBits[2]
+			} else if (cmd.startsWith("article detail ")) { 
+				String[] cmdBits = cmd.split(" ");  
 				
 				int id = 0;
 				
@@ -73,7 +74,7 @@ public class Main {
 					}
 				}
 				
-				if (foundArticle == null) { // null 검증
+				if (foundArticle == null) { 
 					System.out.println(id + "번 게시물이 존재하지 않습니다");
 					continue;
 				}
@@ -97,51 +98,61 @@ public class Main {
 					e.printStackTrace();
 				}
 				
-				Article foundArticle = null;  // 재활용과 null 검증을 위해
+				Article foundArticle = null;  
 				
 				for (Article article : articles) {
 					if (article.id == id) {
-						foundArticle = article; // 객체지향프로그래밍 , 무조건 이용
+						foundArticle = article; 
 						break;
 					}
 				}
 				
-				//향상된 for문 이용 + foundIndex 이용 
-//			    int foundIndex = -1 ; // null과 같은 의미로 -1 로 초기화 (존재하지 않는 index 의미)
-//
-//			    int indexId = 0;
-//			    
-//			    for(Article article : articles) {
-//			    	if (article.id == id) {
-//			    		foundIndex = indexId;
-//			    		foundArticle = article; // 누락된 명령어, null 검증 위해 필요
-//			    		break;
-//			    	}
-//			    	indexId++;
-//			    }
-				
-			    // 일반 for문 이용 + foundIndx 이용 
-//			    int foundIndex = -1 ; // null과 같은 의미로 -1 로 초기화 (존재하지 않는 index 의미)
-//				
-//				for (int i = 0; i < articles.size(); i++) {
-//					Article article =articles.get(i);
-//					if (article.id == id) {
-//						foundArticle = article;  // 누락된 명령어, null 검증 위해 필요
-//						foundIndex = i;
-//						break;
-//					}
-//				}
-				
-				// null 검증 
 				if (foundArticle == null) { 
 					System.out.println(id + "번 게시물이 존재하지 않습니다");
 					continue;
 				}
 				
-				articles.remove(foundArticle);   // 2가지 방법이 있지요. 첫번째 : 객체 이용
-//				articles.remove(foundIndex);     // 두번쨰 : 인덱스 이용, foundIndex 이용하는 방법(2가지도)도 주석처리
+				articles.remove(foundArticle);   
 				
 				System.out.println(id + "번 게시물이 삭제 되었습니다");
+				
+			} else if (cmd.startsWith("article modify ")) { 
+				String[] cmdBits = cmd.split(" ");  
+				
+				int id = 0;  
+				
+				try {
+					id = Integer.parseInt(cmdBits[2]);  
+				} catch (NumberFormatException e) {
+					System.out.println("명령어가 올바르지 않습니다");
+					continue;
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				Article foundArticle = null;  
+				
+				for (Article article : articles) {
+					if (article.id == id) {
+						foundArticle = article;   // 중요 ==> 주소 복사
+						break;
+					}
+				}
+				
+				if (foundArticle == null) { 
+					System.out.println(id + "번 게시물이 존재하지 않습니다");
+					continue;
+				}
+				
+				System.out.println("수정할 제목 :");
+				String title = sc.nextLine().trim();
+				System.out.println("수정할 내용 :");
+				String body = sc.nextLine().trim();
+				
+				foundArticle.title = title;  // 수정된 값을 객체에다 저장 => 수정				foundArticle.body = body;
+				
+				
+				System.out.println(id + "번 게시물이 수정 되었습니다");
 				
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다");
